1454329bf129cbf4a12bc14831b00fb98da21c83
weisuhua <51103527+weisuhua@users.noreply.github.com>
2019-06-04 10:27:13 +0800

    Update main.py
    the first!

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import json
+import ujson
+import time
+import MQTT
+import machine
+
+print('***************adding from pythonA')
+print('**************this is a demo ******************')
+print('**************master adding ******************')
+print('**************adding now*******************')
+print('**************adding from master branch')
+print('**************adding from pythonD branch*************')
+print('**************adding from weisuhua*************')
